a5a524f25d7902e20983134b082087f229460d23
MatPaluch <xp.mateusz@gmail.com>
2024-04-07 17:33:00 +0200

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const link = document.querySelector('.gallery');
+
+for (const obj of galleryItems) {
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+
+  const anchor = document.createElement('a');
+  anchor.classList.add('gallery__link');
+  anchor.setAttribute('href', obj.original);
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = obj.preview;
+  image.setAttribute('data-source', obj.original);
+  image.title = obj.description;
+
+  link.appendChild(li);
+  li.appendChild(anchor);
+  anchor.appendChild(image);
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+link.addEventListener('click', event => {
+  event.preventDefault();
+});
