150ae85be79294bd47a2f62de4f0bca9d1a138c8
MatPaluch <xp.mateusz@gmail.com>
2024-04-07 20:30:36 +0200

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  const time = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(time);
+}
+player.on(
+  'timeupdate',
+  _.throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import _ from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name=email]');
+
+const data = {
+  email: '',
+  message: '',
+};
+console.log(email);
+const dataStorage = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (dataStorage.email !== '') {
+  email.value = dataStorage.email;
+}
+
+form.addEventListener('input', e => {
+  if (e.target.name === 'email') {
+    data.email = e.target.value;
+  }
+  if (e.target.name === 'message') {
+    data.message = e.target.value;
+  }
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+});
